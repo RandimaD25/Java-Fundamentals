3bcf52823f02ad92686654e4266c8ef6fe02917e
RandimaD25 <randimamethminid525@gmail.com>
2025-06-06 19:15:51 +0530

    feat: java fundamentals

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("I like pizza\n");
+        System.out.println(("It is really good"));
+
+        int age = 26;
+        System.out.println(age);
+
+        int year = 2025;
+        System.out.println("This year is " + year);
+
+        double price = 19;
+        System.out.println("$" + price);
+
+        char result = 'A';
+        char currency = '$';
+        System.out.println(result);
+
+        boolean isSell = true;
+        System.out.println(isSell);
+
+        String name = "Randima";
+        System.out.println(name);
+    }
+}
