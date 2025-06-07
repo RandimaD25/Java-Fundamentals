a91df1c5a1a910e63ddecb1bef6987b3195eda17
RandimaD25 <randimamethminid525@gmail.com>
2025-06-07 11:23:55 +0530

    feat: user input

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,69 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("I like pizza\n");
-        System.out.println(("It is really good"));
+//        System.out.print("I like pizza\n");
+//        System.out.println(("It is really good"));
+//
+//        int age = 26;
+//        System.out.println(age);
+//
+//        int year = 2025;
+//        System.out.println("This year is " + year);
+//
+//        double price = 19;
+//        System.out.println("$" + price);
+//
+//        char result = 'A';
+//        char currency = '$';
+//        System.out.println(result);
+//
+//        boolean isSell = true;
+//        System.out.println(isSell);
+//
+//        String name = "Randima";
+//        System.out.println(name);
 
-        int age = 26;
-        System.out.println(age);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter your age: ");
+//        int age = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        System.out.print("Enter your name: ");
+//        String name = scanner.nextLine();
 
-        int year = 2025;
-        System.out.println("This year is " + year);
 
-        double price = 19;
-        System.out.println("$" + price);
+//        System.out.print("Enter your gpa: ");
+//        double gpa = scanner.nextDouble();
+//
+//        System.out.print("Are you a student: ");
+//        boolean isStudent = scanner.nextBoolean();
 
-        char result = 'A';
-        char currency = '$';
-        System.out.println(result);
+//        System.out.println("You are " + age + " years old");
+//        System.out.println("Hello " + name);
+//        System.out.println("Your gpa is " + gpa);
+//
+//        if (isStudent) {
+//            System.out.println("You are a student");
+//        } else {
+//            System.out.println("You are NOT a student");
+//        }
 
-        boolean isSell = true;
-        System.out.println(isSell);
+//        scanner.close();
 
-        String name = "Randima";
-        System.out.println(name);
+//        calculate area of the rectangle
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the width: ");
+        double width = scanner.nextDouble();
+
+        System.out.print("Enter your height: ");
+        double height = scanner.nextDouble();
+
+        double area = width * height;
+        System.out.println("Area is: " + area + "cm²");
+
+        scanner.close();
     }
 }
