6fb19d027e0a755d0ab4d87a1bd0965008a41824
RandimaD25 <randimamethminid525@gmail.com>
2025-07-03 18:56:15 +0530

    feat: overloaded constructors

--- src/Main.java
+++ src/Main.java
@@ -16,15 +16,37 @@ public class Main {
 //
 //        car.drive();
 
-        Student student1 = new Student("Randima", 26, 3.36);
-        Student student2 = new Student("Sara", 25, 3.33);
-        student1.isEnrolled = false;
+//        Student student1 = new Student("Randima", 26, 3.36);
+//        Student student2 = new Student("Sara", 25, 3.33);
+//        student1.isEnrolled = false;
+//
+//        System.out.println(student1.name);
+//        System.out.println(student1.isEnrolled);
+//
+//        student1.study();
+//        student2.study();
 
-        System.out.println(student1.name);
-        System.out.println(student1.isEnrolled);
+        User user = new User("Randima");
+        User user1 = new User("Sara", "sara@gmail.com");
+        User user2 = new User("Sandy", "sandy@gmail.com", 24);
+        User user3 = new User();
 
-        student1.study();
-        student2.study();
+        System.out.println(user.username);
+        System.out.println(user.email);
+        System.out.println(user.age);
+
+        System.out.println(user1.username);
+        System.out.println(user1.email);
+        System.out.println(user1.age);
+
+        System.out.println(user2.username);
+        System.out.println(user2.email);
+        System.out.println(user2.age);
+
+        System.out.println(user3.username);
+        System.out.println(user3.email);
+        System.out.println(user3.age);
+
     }
 }
 
