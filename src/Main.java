04f2b7b44bf520b0873fd2474b04d791383d6086
RandimaD25 <randimamethminid525@gmail.com>
2025-07-03 18:19:31 +0530

    feat: constructors

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,28 @@ public class Main {
 
 //        car.isRunning = true;
 
-        System.out.println(car.make);
-        System.out.println(car.model);
-        System.out.println(car.price);
-        System.out.println(car.isRunning);
+//        System.out.println(car.make);
+//        System.out.println(car.model);
+//        System.out.println(car.price);
+//        System.out.println(car.isRunning);
+//
+//        car.start();
+//        System.out.println(car.isRunning);
+//        car.stop();
+//        System.out.println(car.isRunning);
+//
+//        car.drive();
 
-        car.start();
-        System.out.println(car.isRunning);
-        car.stop();
-        System.out.println(car.isRunning);
+        Student student1 = new Student("Randima", 26, 3.36);
+        Student student2 = new Student("Sara", 25, 3.33);
+        student1.isEnrolled = false;
 
-        car.drive();
+        System.out.println(student1.name);
+        System.out.println(student1.isEnrolled);
+
+        student1.study();
+        student2.study();
     }
 }
+
+
