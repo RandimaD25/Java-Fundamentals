868ec1079bdd85308decac2270641a5318f69045
RandimaD25 <randimamethminid525@gmail.com>
2025-09-11 18:58:46 +0530

    feat: Multithreading

---
+++ src/ARunnable.java
@@ -0,0 +1,20 @@
+public class ARunnable implements Runnable {
+
+    private final String text;
+
+    ARunnable(String text) {
+        this.text = text;
+    }
+    @Override
+    public void run () {
+        for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(1000);
+//                System.out.println(Thread.currentThread().getName() + " " + i);
+                System.out.println(text);
+            } catch (InterruptedException e) {
+                System.out.println("Thread was interrupted");
+            }
+        }
+    }
+}
--- src/Threading.java
+++ src/Threading.java
@@ -2,20 +2,49 @@ import java.util.Scanner;
 
 public class Threading {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("You have 5 seconds to enter your name");
+//
+//        MyRunnable myRunnable = new MyRunnable();
+//        Thread thread = new Thread(myRunnable);
+//        thread.setDaemon(true);
+//        thread.start();
+//
+//        System.out.print("Enter your name: ");
+//        String name = scanner.nextLine();
+//
+//        System.out.println("Hello " + name);
+//
+//        scanner.close();
 
-        System.out.println("You have 5 seconds to enter your name");
 
-        MyRunnable myRunnable = new MyRunnable();
-        Thread thread = new Thread(myRunnable);
-        thread.setDaemon(true);
-        thread.start();
 
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
 
-        System.out.println("Hello " + name);
+//        Multithreading
+//        ARunnable aRunnable = new ARunnable();
+//        Thread thread = new Thread(aRunnable);
+//        thread.start();
 
-        scanner.close();
+//        instead of creating aRunnable object, we can pass an anonymous
+//        runnable object to the Thread constructor.
+
+        Thread thread1 = new Thread(new ARunnable("Ping"));
+        Thread thread2 = new Thread(new ARunnable("Pong"));
+
+        System.out.println("GAME START");
+        thread1.start();
+        thread2.start();
+
+//        to finish thread1 and thread2 before printing "GAME OVER"
+//        otherwise it will print first "GAME START" and "GAME OVER"
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread was interrupted.");
+        }
+
+        System.out.println("GAME OVER");
     }
 }
